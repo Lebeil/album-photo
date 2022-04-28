f9b4f695e4e72ff56a8c630fff0fdd542688c3a6
Liedel <liedel75@gmail.com>
2022-04-28 12:37:30 +0200

    add edit

--- src/feature/picture.slice.js
+++ src/feature/picture.slice.js
@@ -13,9 +13,21 @@ export const pictureSlice = createSlice({
             /*classic method*/
             /*state.pictures = [...state.pictures, payload]*/
             state.pictures.push(payload)
+        },
+        editPicture: (state, {payload}) => {
+            state.pictures = state.pictures.map((pic) => {
+                if(pic.id === payload[1]) {
+                    return {
+                        ...pic,
+                        artist: payload[0],
+                    };
+                } else {
+                    return pic;
+                }
+            })
         }
     },
 });
 
-export const { setPicturesData, addPicture } = pictureSlice.actions;
+export const { setPicturesData, addPicture, editPicture } = pictureSlice.actions;
 export default pictureSlice.reducer;
